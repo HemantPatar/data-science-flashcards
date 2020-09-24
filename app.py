6b3604eb69cccdcfcaeefe13393647f3139cb1c5
Raimi Karim (B4D2D7F7) <raimi@aisingapore.org>
2020-09-24 18:55:49 +0800

    Add app and parser

---
+++ app.py
@@ -0,0 +1,39 @@
+import random
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+with open("theory.md", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+quiz = {}
+for i in range(len(lines)):
+
+    # For every line, if it starts with a **,
+    # this starts the beginning of a question-answer pair
+    if lines[i].startswith("**"):
+
+        # Get the question
+        qn = lines[i]
+
+        # The answer is everything in the next few lines
+        # up till a line which starts with a **
+        i += 1
+        start = i
+        ans_lines = []
+        while i < len(lines) and not lines[i].startswith("**"):
+            ans_lines.append(i)
+            i += 1
+        end = i
+
+        # Concat final answer
+        ans = "".join(lines[start:end])
+
+        # Store in dictionary
+        quiz[qn] = ans
+
+
+@app.get("/")
+def index():
+    return random.choice(qns)
